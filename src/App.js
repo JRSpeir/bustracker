2683285e265c6cbc5a42c24ad73c642f889b3f63
jrspeir <jack.speir@hotmail.co.uk>
2023-07-22 00:00:54 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react'
+import refresh from './TfeService.js'
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Table from './Table';
+
+function retriveData(setData)
+{
+  refresh().then(data => setData(data))
+}
+
+export default function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+      retriveData(setData);
+  }, [])
+
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Bus Tracker
+        </Typography>
+        <Table data={data}/>
+      </Box>
+    </Container>
+  );
+}
---
+++ src/TfeService.js
@@ -0,0 +1,42 @@
+export default async function refresh(setData){
+    try {
+        const responseJson = await (await  fetch('https://tfe-opendata.com/api/v1/live_bus_times/36232893', {
+          method: "GET",
+          headers: {
+            "Authorization": process.env.TFE_KEY
+          }
+        })
+        .then(t => t.json())
+        .catch(error => console.log(error)))
+        
+        //setData(extractRows(responseJson));
+        return extractRows(responseJson);
+  
+    } catch (err) {
+        console.log(err.message)
+    }
+  }
+  
+  function extractRows(responseJson)
+  {
+    return responseJson
+    .map(res => res.departures)
+    .map(departures => getRowsFromDepartures(departures))
+    .flat(1)
+    .sort((a, b) => a.time - b.time);
+  }
+  
+  function getRowsFromDepartures(departures)
+  {
+    return departures.map(t => {
+        const timeNow = new Date(Date.now());
+        const depart = new Date(Date.parse(t.departureTime));
+      
+        const diff = Math.round(((depart - timeNow)/1000/60)-60);
+
+        const display = diff === 0 ? "Arrived" : diff;
+    
+        const q = {"route": t.routeName, "time": display};
+        return q;
+    });
+  }
